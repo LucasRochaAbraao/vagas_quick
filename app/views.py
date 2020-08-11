a801edb904327ab2a43331f249296586b0f36b23
Lucas Rocha Abraão <lucasrabraao@gmail.com>
2020-08-10 22:24:13 -0300

    fixed extra dictionary from google sheets

--- app/views.py
+++ app/views.py
@@ -5,8 +5,8 @@ from app.gsheets import service_account
 @app.route('/', methods=['GET'])
 def vagas():
     sheet = service_account.auth("VAGAS QUICK")
-    del sheet[:-1]
+    vagas = service_account.consultar_planilha(sheet)
+    del vagas[:-1]
+
     return jsonify(service_account.consultar_planilha(sheet))
-
-
 
