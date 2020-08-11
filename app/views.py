62bc71513c887745b9c590562963b6c72e75b1d8
Lucas Rocha Abraão <lucasrabraao@gmail.com>
2020-08-10 22:16:19 -0300

    removed last dict that gets created automatically from gsheets

--- app/views.py
+++ app/views.py
@@ -5,6 +5,7 @@ from app.gsheets import service_account
 @app.route('/', methods=['GET'])
 def vagas():
     sheet = service_account.auth("VAGAS QUICK")
+    del sheet[:-1]
     return jsonify(service_account.consultar_planilha(sheet))
 
 
